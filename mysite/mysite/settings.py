815be88e59af5983b22e6d07f934e2d01df06436
azad <you@example.com>
2017-05-06 14:04:49 +0600

    Add fend

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -61,7 +61,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'front_app/templates')
+            os.path.join(BASE_DIR, 'front_app/templates'),
+            os.path.join(BASE_DIR, 'front_app/templates/src/')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
